9d80859d6b87624a05da320cfe8ca79024f569bd
Donie Leigh <donie.leigh@gmail.com>
2021-12-14 14:34:30 +0800

    Map key B to the default browser according to the hostname.

--- modules/launcher.lua
+++ modules/launcher.lua
@@ -8,27 +8,43 @@ local fnutils = require "hs.fnutils"
 grid.setMargins({0, 0})
 
 applist = {
-    {shortcut = '1',appname = 'OmniFocus'},
-    {shortcut = '2',appname = 'flomo'},
-    {shortcut = 'A',appname = 'Sequel Ace'},
-    {shortcut = 'B',appname = 'Brave Browser'},
-    {shortcut = 'C',appname = 'Visual Studio Code'},
-    {shortcut = 'D',appname = 'Dash'},
-    {shortcut = 'E',appname = 'EuDic'},
-    {shortcut = 'F',appname = 'Firefox'},
-    {shortcut = 'G',appname = 'Google Chrome'},
-    {shortcut = 'L',appname = 'Telegram'},
-    {shortcut = 'M',appname = 'Mail'},
-    {shortcut = 'N',appname = 'Notion'},
-    {shortcut = 'O',appname = 'Microsoft Outlook'},
-    {shortcut = 'P',appname = 'PhpStorm'},
-    {shortcut = 'Q',appname = 'Activity Monitor'},
-    {shortcut = 'S',appname = 'Slack'},
-    {shortcut = 'T',appname = 'Tweetbot'},
-    {shortcut = 'V',appname = 'MacVim'},
-    {shortcut = 'W',appname = 'Workflowy'},
+    {shortcut = '1', appname = 'OmniFocus'},
+    {shortcut = '2', appname = 'flomo'},
+    {shortcut = 'A', appname = 'Sequel Ace'},
+    {shortcut = 'C', appname = 'Visual Studio Code'},
+    {shortcut = 'D', appname = 'Dash'}, {shortcut = 'E', appname = 'EuDic'},
+    {shortcut = 'F', appname = 'Firefox'},
+    {shortcut = 'L', appname = 'Telegram'}, {shortcut = 'M', appname = 'Mail'},
+    {shortcut = 'N', appname = 'Notion'},
+    {shortcut = 'O', appname = 'Microsoft Outlook'},
+    {shortcut = 'P', appname = 'PhpStorm'},
+    {shortcut = 'Q', appname = 'Activity Monitor'},
+    {shortcut = 'S', appname = 'Slack'}, {shortcut = 'T', appname = 'Tweetbot'},
+    {shortcut = 'V', appname = 'MacVim'},
+    {shortcut = 'W', appname = 'Workflowy'}
 }
 
+--[[
+   [ Map key B to the default browser.
+   ]]
+local defaultBrowser = nil
+local hostnames = hs.host.names()
+
+for i = 1, #hostnames do
+    if string.find(string.lower(hostnames[i]), 'imac') ~= nil then
+        defaultBrowser = 'Google Chrome'
+        break
+    elseif string.find(string.lower(hostnames[i]), 'mbp') ~= nil then
+        defaultBrowser = 'Brave Browser'
+        break
+    end
+end
+
+if defaultBrowser ~= nil then
+    table.insert(applist, {shortcut = 'B', appname = defaultBrowser})
+end
+
+-- Do mappings.
 fnutils.each(applist, function(entry)
     -- hotkey.bind({'ctrl', 'shift'}, entry.shortcut, entry.appname, function()
     hotkey.bind({'alt'}, entry.shortcut, entry.appname, function()
