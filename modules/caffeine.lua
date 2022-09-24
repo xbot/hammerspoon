e563fad5952221a8f63f1b0d5d61a14aeaf8b436
Donie Leigh <donie.leigh@gmail.com>
2022-09-24 12:18:17 +0800

    Optimize code.

--- modules/caffeine.lua
+++ modules/caffeine.lua
@@ -4,7 +4,7 @@
 
 local menubar_item = nil
 
-local toggle_caffeine = function()
+local function initialize_caffeine()
     if Settings ~= nil and Settings[1].caffeine == 'on' and menubar_item == nil then
         menubar_item = hs.menubar.new()
         menubar_item:setTitle('')
@@ -25,11 +25,7 @@ local function reset_menubar_item()
     end
 end
 
-local function run()
-    toggle_caffeine()
+initialize_caffeine()
 
-    -- Check caffeine status, decide whether to reset the menubar item
-    hs.timer.doEvery(1, reset_menubar_item)
-end
-
-run()
+-- Check caffeine status, decide whether to reset the menubar item
+hs.timer.doEvery(1, reset_menubar_item)
