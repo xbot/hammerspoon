a34c2cd0ac587d5a0698c0c2901015a2aba2d896
Donie Leigh <donie.leigh@gmail.com>
2023-04-18 14:45:14 +0800

    Won't apply layout to windows which are manually moved.

--- modules/desktop_layout.lua
+++ modules/desktop_layout.lua
@@ -5,6 +5,16 @@
 local logger = hs.logger.new('Launcher', 'debug')
 
 local previousScreenByWindow = {}
+local moveTypeByWindow = {}
+
+-- Initiate previousScreenByWindow on startup
+for _, app in ipairs(hs.application.runningApplications()) do
+    for _, window in ipairs(app:allWindows()) do
+        if window:isStandard() and not window:isMinimized() then
+            previousScreenByWindow[window:id()] = window:screen():getUUID()
+        end
+    end
+end
 
 -- Take the right most screen as the secondary screen.
 local function get_secondary_screen()
@@ -59,7 +69,8 @@ local desktopLayout = {
         frame = hs.geometry.rect(0.33, 0, 0.67, 1),
         layouts = {
             {screen = get_secondary_screen(), frame = nil}
-        }
+        },
+        excludeWindows = {'.* Preferences'}
     },
     -- The top half of the secondary screen, fallback to the left a third of the primary screen.
     {apps = {'Slack'}, screen = get_secondary_screen(), frame = hs.geometry.rect(0, 0, 1, 0.5), fallback = {screen = hs.screen.primaryScreen(), frame = hs.geometry.rect(0, 0, 0.33, 1)}},
@@ -80,6 +91,14 @@ end
 -- Move window to designated position
 local function apply_layout(window, layout)
     if not layout then
+        logger.d('Leave window "' .. window:title() .. '" (' .. window:id() .. ') stay put.')
+        return
+    end
+
+    logger.d('Apply layout `' .. hs.inspect(layout, {newline = '', indent = ' '}) .. '` for window "' .. window:title() .. '" (' .. window:id() .. ').')
+
+    if moveTypeByWindow[window:id()] == 'manually' then
+        logger.d('Window "' .. window:title() .. '" is ignored for being manually resized.')
         return
     end
 
@@ -93,7 +112,7 @@ local function apply_layout(window, layout)
     end
 
     if shouldExclude then
-        logger.d('Window "' .. window:title() .. '" ignored by the "excludeWindows" patterns.')
+        logger.d('Window "' .. window:title() .. '" is ignored by the "excludeWindows" patterns.')
         return
     end
 
@@ -111,7 +130,9 @@ local function apply_layout(window, layout)
 
     previousScreenByWindow[window:id()] = window:screen():getUUID()
 
-    logger.d('Placed ' .. window:application():name() .. ' (' .. window:title() .. ') to the ' .. hs.inspect(layout.frame) .. ' of the ' .. layout.screen:name())
+    moveTypeByWindow[window:id()] = 'automatically'
+
+    logger.d('Placed ' .. window:application():name() .. ' (' .. window:title() .. ') to the ' .. hs.inspect(layout.frame, {newline = '', indent = ' '}) .. ' of the ' .. layout.screen:name())
 end
 
 -- Get config from desktopLayout by app name
@@ -125,7 +146,7 @@ local function get_app_config(appName)
     return nil
 end
 
-local function generate_layout(appConfig, screen)
+local function generate_layout(appConfig, screen, event)
     local layout = hs.fnutils.copy(appConfig)
 
     layout.fallback = nil
@@ -139,22 +160,35 @@ local function generate_layout(appConfig, screen)
                 layout.frame = screenLayout.frame
                 layout.center = screenLayout.center
 
+                logger.d('Hit pre-defined layout on screen ' .. screen:name() .. '.')
+
                 return layout
             end
         end
     end
 
-    if not layout.screen and appConfig.fallback then
-        if appConfig.fallback.screen then
-            layout.screen = appConfig.fallback.screen
+    if event == hs.window.filter.windowMoved or event == hs.application.watcher.activated then
+        -- Return nil when the default screen is not nil and not the current screen.
+        if appConfig.screen and appConfig.screen ~= screen then
+            logger.d('The window is activated or moved or resized on the non-default screen ' .. screen:name() .. ', return nil for the layout.')
+            return nil
         end
+    else
+        -- Use the fallback configuration when the default screen is nil.
+        if not appConfig.screen and appConfig.fallback then
+            if appConfig.fallback.screen then
+                layout.screen = appConfig.fallback.screen
+            end
 
-        if appConfig.fallback.frame then
-            layout.frame = appConfig.fallback.frame
-        end
+            if appConfig.fallback.frame then
+                layout.frame = appConfig.fallback.frame
+            end
 
-        if appConfig.fallback.center then
-            layout.center = appConfig.fallback.center
+            if appConfig.fallback.center then
+                layout.center = appConfig.fallback.center
+            end
+
+            logger.d('Use the fallback configuration.')
         end
     end
 
@@ -165,27 +199,48 @@ end
 
 local wf = hs.window.filter.default
 
-wf:subscribe(hs.window.filter.windowCreated, function(window, appName)
+wf:subscribe(hs.window.filter.windowCreated, function(window, appName, event)
     if not window:isStandard() or window:isMinimized() then
         return
     end
 
-    logger.d('New window "' .. window:title() .. '" created for ' .. appName)
+    logger.d('New window "' .. window:title() .. '" (' .. window:id() .. ') created for ' .. appName)
+
+    previousScreenByWindow[window:id()] = window:screen():getUUID()
 
     local config = get_app_config(appName)
     if not config then
         return
     end
 
-    local layout = generate_layout(config, window:screen())
-    apply_layout(window, layout)
+    apply_layout(window, generate_layout(config, window:screen(), event))
+
+    -- The windowMoved event won't be triggered here.
+    moveTypeByWindow[window:id()] = nil
 end)
 
-wf:subscribe(hs.window.filter.windowMoved, function(window)
+wf:subscribe(hs.window.filter.windowMoved, function(window, appName, event)
+    if not window:isStandard() or window:isMinimized() then
+        return
+    end
+
+    if not moveTypeByWindow[window:id()] then
+        moveTypeByWindow[window:id()] = 'manually'
+    end
+
+    logger.d("Window " .. window:title() .. " (" .. window:id() .. ") has been moved " .. moveTypeByWindow[window:id()] .. ".")
+
+    if moveTypeByWindow[window:id()] == 'automatically' then
+        moveTypeByWindow[window:id()] = nil
+    end
+
     local prevScreenUUID = previousScreenByWindow[window:id()]
     local newScreenUUID = window:screen():getUUID()
 
     if newScreenUUID ~= prevScreenUUID then
+        -- Apply the corresponding layout when a window is manually moved to another screen.
+        moveTypeByWindow[window:id()] = nil
+
         local config = get_app_config(window:application():name())
         if not config then
             return
@@ -193,9 +248,15 @@ wf:subscribe(hs.window.filter.windowMoved, function(window)
 
         logger.d("Window " .. window:title() .. " has been moved to screen: " .. window:screen():name())
 
-        local layout = generate_layout(config, window:screen())
-        apply_layout(window, layout)
+        apply_layout(window, generate_layout(config, window:screen(), event))
+
+        previousScreenByWindow[window:id()] = newScreenUUID
     end
+end)
+
+wf:subscribe(hs.window.filter.windowDestroyed, function(window)
+    moveTypeByWindow[window:id()] = nil
+    previousScreenByWindow[window:id()] = nil
 end)
 
 -- Application watcher
@@ -211,8 +272,17 @@ AppWatcher = hs.application.watcher.new(function(appName, eventType, appObject)
     end
 
     wait_for_window_ready(appObject, function()
-        local layout = generate_layout(config, appObject:focusedWindow():screen())
-        apply_layout(appObject:focusedWindow(), layout)
+        local window = appObject:focusedWindow()
+
+        if not window:isStandard() or window:isMinimized() then
+            return
+        end
+
+        logger.d('The activated event is triggered for app "' .. appName .. '" window "' .. window:title() .. '" (' .. window:id() .. ').')
+
+        previousScreenByWindow[window:id()] = window:screen():getUUID()
+
+        apply_layout(window, generate_layout(config, window:screen(), eventType))
     end)
 end)
 
